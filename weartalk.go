c16b708c54757927543fe87b126c84017d72435a
AkinoKaede <autmaple@protonmail.com>
2020-03-19 18:34:57 +0800

    HTTP GET Method

--- weartalk.go
+++ weartalk.go
@@ -1,12 +1,18 @@
 package main
 
-import "strconv"
+import (
+	"strconv"
+
+	"github.com/valyala/fasthttp"
+)
 
 type WearTalk struct {
 	UID      int
 	NickName string
 	Sex      int8
 	Key      string
+	Device   int8
+	XFF      string
 }
 
 func (wt *WearTalk) getNickName() string {
@@ -23,5 +29,30 @@ func (wt *WearTalk) getNickName() string {
 }
 
 func (wt *WearTalk) getUID() string {
-	return "TaMP" + strconv.Itoa(wt.UID)
+	switch wt.Device {
+	case 0:
+		return "TaMi" + strconv.Itoa(wt.UID)
+	case 1:
+		return "TaMP" + strconv.Itoa(wt.UID)
+	default:
+		return "Ta" + strconv.Itoa(wt.UID)
+	}
+}
+
+func (wt *WearTalk) Get(url string, args *fasthttp.Args) ([]byte, error) {
+	req := &fasthttp.Request{}
+	req.SetRequestURI(url + "?" + args.String())
+	req.Header.SetMethod("GET")
+	req.Header.SetUserAgent("Dalvik/2.1.0 (Linux; U; Android 5.1.1; PRO 6 Plus Build/LMY48Z)")
+	req.Header.Set("Accept-Encoding", "gzip")
+	if wt.XFF != "" {
+		req.Header.Set("X-Forwarded-For", wt.XFF)
+	}
+
+	resp := &fasthttp.Response{}
+	client := fasthttp.Client{}
+
+	err := client.Do(req, resp)
+
+	return resp.Body(), err
 }
