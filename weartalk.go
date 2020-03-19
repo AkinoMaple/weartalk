c0cd91330349518381e42571c7a38d548c124364
AkinoKaede <autmaple@protonmail.com>
2020-03-19 19:38:14 +0800

    Get Version

--- weartalk.go
+++ weartalk.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"strconv"
 
 	"github.com/valyala/fasthttp"
@@ -13,6 +15,12 @@ type WearTalk struct {
 	Key      string
 	Device   int8
 	XFF      string
+}
+
+type Version struct {
+	Version string `json:"version"`
+	Tips    string `json:"tips"`
+	URL     string `json:"url"`
 }
 
 func (wt *WearTalk) getNickName() string {
@@ -44,15 +52,34 @@ func (wt *WearTalk) Get(url string, args *fasthttp.Args) ([]byte, error) {
 	req.SetRequestURI(url + "?" + args.String())
 	req.Header.SetMethod("GET")
 	req.Header.SetUserAgent("Dalvik/2.1.0 (Linux; U; Android 5.1.1; PRO 6 Plus Build/LMY48Z)")
+	req.Header.Set("Accept", "*/*")
 	req.Header.Set("Accept-Encoding", "gzip")
 	if wt.XFF != "" {
 		req.Header.Set("X-Forwarded-For", wt.XFF)
 	}
 
 	resp := &fasthttp.Response{}
-	client := fasthttp.Client{}
+	client := &fasthttp.Client{}
 
 	err := client.Do(req, resp)
 
 	return resp.Body(), err
+}
+
+func (wt *WearTalk) GetVersion() *Version {
+	args := &fasthttp.Args{}
+	args.Add("ruanjian", "weartalk参数Ta")
+	resp, rErr := wt.Get("https://zhinengjiaju.vip/xczx/getversion.action", args)
+	if rErr != nil {
+		log.Printf("Get Version Request Error: %s", rErr)
+		return nil
+	}
+
+	var ver Version
+	mErr := json.Unmarshal(resp, &ver)
+	if mErr != nil {
+		log.Printf("Unmarshal Version Data Error: %s", mErr)
+	}
+
+	return &ver
 }
